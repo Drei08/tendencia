12ff284fc3603bbbfb867bf3d80ab3d48e4befe8
Drei08 <andrei_rheivjarth@hotmail.com>
2026-06-22 20:20:32 -0300

    Page Contato

---
+++ src/pages/Contato/Contato.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Contato = () => {
+  return (
+  <section className="contato">
+    <div className="card">
+      <h1>Contato</h1>
+      <a href="https://instagram.com/tendenciaocz" target="_blank" rel="noreferrer">
+      Instagram: @tendenciaocz
+      </a>
+      <br/>
+      <a href="https://wa.me/5518996493403" target="_blank" rel="noreferrer">
+      WhatsApp: (18) 99649-3403
+      </a>
+    </div>
+ </section>
+  )
+}
+
+export default Contato
